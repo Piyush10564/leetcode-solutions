ed0132efe16c6138792812030e4ad596c6f808ae
Piyush singla <147793978+Piyush10564@users.noreply.github.com>
2026-03-25 14:51:03 +0530

    Time: 14 ms (33.64%), Space: 48.4 MB (8.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,19 +2,18 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         Arrays.sort(nums);
         int duplicate=-1;
-        int missing =1;
+        int n= nums.length;
+        int sum=(n*(n+1))/2;
+        int sum2=0;
         for(int i=1;i<nums.length;i++){
             if(nums[i]==nums[i-1]){
                 duplicate = nums[i];
-            }
-            else if(nums[i]>nums[i-1]+1){
-                missing = nums[i-1]+1;
-            }
-           
+            } 
         }
-        if(nums[nums.length-1] != nums.length){
-            missing = nums.length;
-        }
+      for(int i=0;i<nums.length;i++){
+        sum2 += nums[i];
+      }
+      int missing = sum - sum2 + duplicate;
 
         return new int[]{duplicate,missing};
     }
