dbd77d84ea7cf510043a1c44f7be16e54aec9ca7
Piyush singla <147793978+Piyush10564@users.noreply.github.com>
2026-07-23 09:58:53 +0530

    Time: 487 ms (17.88%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,9 +1,4 @@
-# Write your MySQL query statement below
-SELECT DISTINCT
-    author_id AS id
-FROM
-    Views
-WHERE
-   author_id= viewer_id
-ORDER BY 
-id ASC;
+select distinct(author_id) as id
+from Views
+where author_id = viewer_id
+order by id asc;
